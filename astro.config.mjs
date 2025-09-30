5f8372eb5093d5e14c2f0746cc28c4586538220c
Grayson West <patternflux@users.noreply.github.com>
2025-09-30 09:55:00 -0700

    changing astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.particlebunni.com',
-  // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
+  base: '/',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
